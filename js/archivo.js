79902d69b06c42bc1730f683ee382e4b6b99ab59
aagustindi <aagustindi@gmail.com>
2023-03-23 13:30:14 -0300

    Se corrigió el condicional del case, con un nuevo case en la misma linea

--- js/archivo.js
+++ js/archivo.js
@@ -1,68 +1,79 @@
 let total = 0
-deporte= "";
-dia="";
+deporte = "";
+dia = "";
 
 
 reservarCancha();
 
-function reservarCancha(){
+function reservarCancha() {
 
     alert(
         "Bienvenido a Reserva tu cancha!\n Selecciona el Deporte\n 1- Futbol - $11000 \n 2- Padel - $4000 \n 3- Ajedrez - $1000  "
     );
 
-    let deporte = prompt("Elegí el Deporte: Futbol, Padel o Ajedrez?").toLowerCase(); 
+    let deporte = prompt("Elegí el Deporte: Futbol, Padel o Ajedrez?").toLowerCase();
 
 
-    switch (deporte){
-        case ("futbol" || "1"): 
-        alert("OK Messi, Empecemos");
-        total += 11000
-        alert("El total sería: $ " + total + "!");  
-        reservarDia();
-        alert("Listo, reservaste para el: " + dia + " para jugar al: " + deporte )
-        alert ("Juntate los $ " + total + " y las ganas de correr")
-        break;
+    switch (deporte) {
+        case ("1"):
+            deporte = "Futbol"
+        case ("futbol"):
+            alert("OK Messi, Empecemos");
+            total += 11000
+            alert("El total sería: $ " + total + "!");
+            reservarDia();
+            alert("Listo, reservaste para el: " + dia + " para jugar al: " + deporte)
+            alert("Juntate los $ " + total + " y las ganas de correr")
+            break;
 
-        case ("padel" || "2"): 
-        alert("Padel! Buena elección ");
-        total += 4000;
-        alert("El Total sería: $  " + total + "!"); 
-        reservarDia();
-        alert("Listo, reservaste para el: " + dia + " para jugar al: " + deporte)
-        alert ("Juntate los $ " + total + " :)  Gracias")
-        break;
+        case ("2"):
+            deporte = "Padel"
+        case ("padel"):
+            alert("Padel! Buena elección ");
+            total += 4000;
+            alert("El Total sería: $  " + total + "!");
+            reservarDia();
+            alert("Listo, reservaste para el: " + dia + " para jugar al: " + deporte)
+            alert("Juntate los $ " + total + " :)  Gracias")
+            break;
 
-        case ("ajedrez" || "3"): 
-        alert("Deporte? ok.. ");
-        total += 1000;
-        alert("El Total sería: " + total + "!");  
-        reservarDia()
-        alert("Listo, reservaste para el: " + dia + " para jugar al: " + deporte) 
-        alert ("Juntate los $ " + total + " :) Y preparate para pensar")
-        break;
+        case ("3"):
+            deporte = "Ajedrez" 
+        case ("ajedrez"):
+            alert("Deporte? ok.. ");
+            total += 1000;
+            alert("El Total sería: " + total + "!");
+            reservarDia()
+            alert("Listo, reservaste para el: " + dia + " para jugar al: " + deporte)
+            alert("Juntate los $ " + total + " :) Y preparate para pensar")
+            break;
 
-        default: 
-        alert("Ese deporte aun no lo tenemos, seleccione un deporte de la lista!");
-        reservarCancha();  
+        default:
+            alert("Ese deporte aun no lo tenemos, seleccione un deporte de la lista!");
+            reservarCancha();
     }
 
 }
 
 
 
-function reservarDia(){
+function reservarDia() {
 
-    alert ("Ya elegiste el deporte, ahora decinos cuando venis \n Sabado o Domingo? ");
+    alert("Ya elegiste el deporte, ahora decinos cuando venis \n Sabado o Domingo? ");
     dia = prompt("Ingresa el dia de la reserva").toLowerCase();
 
-    switch (dia){
+    switch (dia) {
+        case ("sab"):
+            dia= " Sabado"
         case ("sabado"):
+        
             alert("Gracias ! Reservaste para el Sabado");
             break;
-        
+
+        case ("dom"):
+            dia = "Domingo"  
         case ("domingo"):
-            alert("Gracias Reservaste para el Domingo");
+               alert("Gracias Reservaste para el Domingo");
             break;
 
         default:
